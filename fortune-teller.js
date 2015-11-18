e1b4712ec2b0d9c4ee721fb420f9b4ed46facae1
babakkhosravifar <babak.khosravifar@gmail.com>
2015-11-18 21:56:28 +0000

    fortune teller gives a number of fortunes

--- fortune-teller.js
+++ fortune-teller.js
@@ -1,2 +1,10 @@
 var theFortune = require('./library/fortune').display;
-console.log(theFortune);
+ console.log(process.argv);
+//console.log(theFortune);
+var num = process.argv[process.argv.length-1];
+console.log(num);
+
+for (var i=0;i<num;i++){
+    var theFortune = require('./library/fortune').display;
+    console.log(theFortune());
+}
